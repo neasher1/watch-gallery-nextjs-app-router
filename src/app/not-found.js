3536b6e9a9005b50c94a582df1dd987711c4477d
neasher1 <neasher75@gmail.com>
2023-08-03 18:25:57 +0600

    watch gallery app router

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+
+
+const PageNotFound = () => {
+    return (
+        <div className="flex justify-center items-center text-center h-[100vh]">
+            <h2 className="text-6xl font-semibold text-white">404!! Page Not Found</h2>
+        </div>
+    );
+};
+
+export default PageNotFound;
---
+++ src/app/product/[id]/loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="flex justify-center items-center text-center h-[100vh]">
+            <h2 className="text-6xl font-semibold text-white">Loading...</h2>
+        </div>
+    );
+};
+
+export default Loading;
